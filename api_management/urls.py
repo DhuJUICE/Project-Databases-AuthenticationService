59846bd8d8a96676fd07c3d20434edb29a5cada9
DhuJUICE <102233050+DhuJUICE@users.noreply.github.com>
2025-09-07 01:50:53 +0200

    updated service to only focus on authentication

--- api_management/urls.py
+++ api_management/urls.py
@@ -8,7 +8,4 @@ urlpatterns = [
 	#Used for logging users in - No need for the login endpoint
 	path('api/token', TokenObtainPairView.as_view(), name='token'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
-
-	#USER MANAGEMENT API ENDPOINTS
-	path('api/register', views.Register.as_view(), name='api-register'),
 ]
