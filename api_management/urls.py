ebd8debfed1b974688d48a739b04064404dddfde
DhuJUICE <102233050+DhuJUICE@users.noreply.github.com>
2025-10-05 02:07:04 +0200

    updated service

--- api_management/urls.py
+++ api_management/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
 	#Used for logging users in - No need for the login endpoint
-	path('api/token', TokenObtainPairView.as_view(), name='token'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+	#path('api/token', TokenObtainPairView.as_view(), name='token'),
+    #path('api/token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
+	path('api/token', views.MyTokenObtainPairView.as_view(), name='token'),
 ]
